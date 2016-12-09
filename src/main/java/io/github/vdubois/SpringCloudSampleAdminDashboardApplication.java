6b837ca903a2b77095b7bd58e86f9b9bd068e0c2
Vincent Dubois <dubois.vct@free.fr>
2016-12-09 08:21:42 +0100

    first commit

---
+++ src/main/java/io/github/vdubois/SpringCloudSampleAdminDashboardApplication.java
@@ -0,0 +1,16 @@
+package io.github.vdubois;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringCloudSampleAdminDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSampleAdminDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/vdubois/SpringCloudSampleAdminDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.vdubois;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudSampleAdminDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
